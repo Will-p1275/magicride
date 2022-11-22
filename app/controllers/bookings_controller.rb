3d1ff3324d4e3af5b9b852608ffc30cc8b6ec3f4
charlotte oswald <charlotteoswald9@gmail.com>
2022-11-22 11:32:46 +0100

    controller with new and create, view of new created, but waiting for DB to be created to test it

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,26 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @ride = Ride.find(params[:id])
+    @booking = Booking.new(booking_params)
+    days = booking_params[:end_date] - booking_params[:starting_date]
+    total_price = days * @ride.price_per_day
+    @booking.total_price = total_price
+    @booking.ride = @ride
+    @booking.user = current_user
+    if @booking.save
+      redirect_to dashboard_path
+    else
+      render new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :end_date)
+  end
 end
