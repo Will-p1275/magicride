eee2c7df9c1c3a4a46ba5a41946e4c3b00c6952a
charlotte oswald <charlotteoswald9@gmail.com>
2022-11-22 12:25:36 +0100

    render new corrected

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -14,7 +14,7 @@ class BookingsController < ApplicationController
     if @booking.save
       redirect_to dashboard_path
     else
-      render new
+      render :new, status: :unprocessable_entity
     end
   end
 
