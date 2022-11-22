a56a16ab58277d3efce052961b76ccaec945af4e
william passera <passeraw@gmail.com>
2022-11-22 10:53:46 +0100

    rides controller and views

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,2 +1,29 @@
 class RidesController < ApplicationController
+  def index
+    @rides = Ride.all
+  end
+
+  def new
+    @ride = Ride.new
+  end
+
+  def create
+    @ride = Ride.new(ride_params)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def ride_params
 end
