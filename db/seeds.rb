38b9a98eac4c64073d13efdc5fbc4f70975fb826
anne rubio <rubio.anne@gmail.com>
2022-11-25 12:37:46 +0100

    added pictures to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -51,7 +51,8 @@ unicorn = Ride.create(
   address: '120, rue de paris 92100 Boulogne',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://www.superprof.fr/blog/wp-content/uploads/2021/03/dessin-illustration-licorne.jpg'
 )
 
 dragon = Ride.create(
@@ -61,7 +62,8 @@ dragon = Ride.create(
   address: '12, rue Oberkampf 75011 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://m.media-amazon.com/images/I/61+FHcqEOYL.jpg'
 )
 
 troll = Ride.create(
@@ -71,18 +73,20 @@ troll = Ride.create(
   address: '50, rue Robespierre 93100 Montreuil',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://p.turbosquid.com/ts-thumb/Qv/Vx9E3b/LO/p2.jpgdff35091b4d14345a606d2f18e4d47d1zoom/jpg/1651332373/600x600/fit_q87/16ce572e89a392623925200466eb8a6bc4c5c82d/p2.jpgdff35091b4d14345a606d2f18e4d47d1zoom.jpg'
 )
 
-    ninjaTurtle = Ride.create(
-      name: 'Greeny Ninja Turle',
-      category: 'Magical Creatures',
-      description: 'Ride Turtle Ninja Greeny. Greeny is a true clone from the tv-series. It has been created and brought to life by a scientist expert in creating magical creatures from movies and tv-series.',
-      address: '50, avenue Pasteur 93100 Montreuil',
-      availability: true,
-      price_per_day: rand(50..100),
-      user: users.sample
-    )
+ninjaTurtle = Ride.create(
+  name: 'Greeny Ninja Turle',
+  category: 'Magical Creatures',
+  description: 'Ride Turtle Ninja Greeny. Greeny is a true clone from the tv-series. It has been created and brought to life by a scientist expert in creating magical creatures from movies and tv-series.',
+  address: '50, avenue Pasteur 93100 Montreuil',
+  availability: true,
+  price_per_day: rand(50..100),
+  user: users.sample,
+  photo: 'https://fr.web.img6.acsta.net/newsv7/20/07/01/12/28/1133031.jpg'
+)
 
 werewolf = Ride.create(
   name: 'Werewolf',
@@ -91,7 +95,8 @@ werewolf = Ride.create(
   address: '50, avenue Pasteur 93100 Montreuil',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/underworld/images/5/59/Kiki.jpg/revision/latest?cb=20110215181201&path-prefix=fr'
 )
 
 sombral = Ride.create(
@@ -101,7 +106,8 @@ sombral = Ride.create(
   address: '18, rue Robespierre 93100 Montreuil',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/harrypotter/images/6/6c/Sombral_2.jpg/revision/latest/scale-to-width-down/799?cb=20100122210256&path-prefix=fr'
 )
 
 totoro = Ride.create(
@@ -111,7 +117,8 @@ totoro = Ride.create(
   address: '25, avenue de la providence 92160 Antony',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://cdn.suwalls.com/wallpapers/anime/my-neighbor-totoro-27257-1920x1080.jpg'
 )
 
 noface = Ride.create(
@@ -121,7 +128,8 @@ noface = Ride.create(
   address: '48, avenue de wagram 75017 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRf8G-SqaVPoC8C4ZTucYEoYpjA1XnVvKVqNA&usqp=CAU'
 )
 
 hedwige = Ride.create(
@@ -131,7 +139,8 @@ hedwige = Ride.create(
   address: '5, rue de la Durance 75012 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://focus.courrierinternational.com/2022/02/05/0/0/1364/588/1280/0/60/0/013ba7c_1644060202638-hedwige.jpg'
 )
 
 nazgul = Ride.create(
@@ -141,7 +150,8 @@ nazgul = Ride.create(
   address: '25, rue de la Seine 75006 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://i.pinimg.com/550x/5c/1b/2b/5c1b2b7748bf1a7f943c351eab636f3b.jpg'
 )
 
 superDestroyer = Ride.create(
@@ -151,7 +161,8 @@ superDestroyer = Ride.create(
   address: '58, avenue Parmentier 75011 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/frstarwars/images/3/30/Executor_BF2.png/revision/latest?cb=20210516202820'
 )
 
 snowspeeder = Ride.create(
@@ -161,7 +172,8 @@ snowspeeder = Ride.create(
   address: '5, rue de Rivoli 75004 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://c-3d.niceshops.com/upload/image/product/large/default/12330_fb2b67f9.jpg'
 )
 
 awing = Ride.create(
@@ -171,7 +183,8 @@ awing = Ride.create(
   address: '10, avenue des Champs-Elysées 75008 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/frstarwars/images/7/7a/A-Wing_RZ-2_BF2.png/revision/latest?cb=20171203154741'
 )
 
 nebulon = Ride.create(
@@ -181,7 +194,8 @@ nebulon = Ride.create(
   address: '12, rue du champs de mars 75007 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://lumiere-a.akamaihd.net/v1/images/nebulon-b-frigate_dce53bc2.jpeg?region=0%2C0%2C1181%2C664&width=960'
 )
 
 constellation = Ride.create(
@@ -191,7 +205,8 @@ constellation = Ride.create(
   address: '42, rue Charcot 75013 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/startrek/images/9/95/StargazerReunion.jpg/revision/latest?cb=20180422014342'
 )
 
 republic = Ride.create(
@@ -201,7 +216,8 @@ republic = Ride.create(
   address: '42, rue Madame 75006 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/135cb28c-5b2b-4d6f-8715-e938fd4de8b2/de75att-969f0602-fe94-4d99-8ce8-db807c8cae14.png/v1/fill/w_1024,h_576,q_80,strp/star_trek__republic_class_federation_starship_by_stingra_de75att-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzEzNWNiMjhjLTViMmItNGQ2Zi04NzE1LWU5MzhmZDRkZThiMlwvZGU3NWF0dC05NjlmMDYwMi1mZTk0LTRkOTktOGNlOC1kYjgwN2M4Y2FlMTQucG5nIiwiaGVpZ2h0IjoiPD01NzYiLCJ3aWR0aCI6Ijw9MTAyNCJ9XV0sImF1ZCI6WyJ1cm46c2VydmljZTppbWFnZS53YXRlcm1hcmsiXSwid21rIjp7InBhdGgiOiJcL3dtXC8xMzVjYjI4Yy01YjJiLTRkNmYtODcxNS1lOTM4ZmQ0ZGU4YjJcL3N0aW5ncmEtNC5wbmciLCJvcGFjaXR5Ijo5NSwicHJvcG9ydGlvbnMiOjAuNDUsImdyYXZpdHkiOiJjZW50ZXIifX0.n_qriX3IJR439DZFxNpOiQ-8O_oZC9KaH0LQ5fPVcrE'
 )
 
 enterprise = Ride.create(
@@ -211,7 +227,8 @@ enterprise = Ride.create(
   address: '42, rue Jacob 75006 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://www.lemagducine.fr/wp-content/uploads/2014/06/Star-Trek-Enterprise-critique-series.jpg'
 )
 
 voyager = Ride.create(
@@ -221,7 +238,8 @@ voyager = Ride.create(
   address: '5, rue Mabillon 75006 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/memoryalpha/images/5/57/Voyager-arri%C3%A8re.jpg/revision/latest?cb=20070702161926&path-prefix=fr'
 )
 
 excalibur = Ride.create(
@@ -231,7 +249,8 @@ excalibur = Ride.create(
   address: '18, rue de la gaité 75014 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.wikia.nocookie.net/stexpanded/images/a/a7/Excalibur.jpg/revision/latest?cb=20080327044217'
 )
 
 yorktown = Ride.create(
@@ -241,7 +260,8 @@ yorktown = Ride.create(
   address: '18, rue Beaurepaire 75010 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://s3.amazonaws.com/files.enjin.com/305813/modules/wiki/34020820/USS_Yorktown_%2898300%29.png'
 )
 
 canoe = Ride.create(
@@ -251,7 +271,8 @@ canoe = Ride.create(
   address: '17, rue de varenne 75007 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://api.wbez.org/v2/images/5c1f164c-d15c-42ae-b94f-b992bf1f6006.jpg?width=1600&height=1066&mode=FILL'
 )
 
 balloon = Ride.create(
@@ -261,7 +282,8 @@ balloon = Ride.create(
   address: '50, avenue Dausmenil 75012 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://media.smartbox.com/pim/1000001967971201770883.jpg'
 )
 
 unicycle = Ride.create(
@@ -271,7 +293,8 @@ unicycle = Ride.create(
   address: '30, avenue Corentin Cariou 75019 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.fnac-static.com/multimedia/Images/A3/A3/C4/2F/3130531-3-1520-2/tsp20201203164902/Monocycle-Qu-Ax-Luxus-20-Chrome.jpg'
 )
 
 carriage = Ride.create(
@@ -281,7 +304,8 @@ carriage = Ride.create(
   address: '30, avenue Mathurin Moreau 75019 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://thumbs.dreamstime.com/z/carrosse-%C3%A0-cheval-vue-arri%C3%A8re-d-une-cal%C3%A8che-traditionnelle-de-l-%C3%A9poque-victorienne-ferm%C3%A9e-avec-deux-chevaux-noirs-sur-all%C3%A9e-161676204.jpg'
 )
 
 hangglider = Ride.create(
@@ -291,7 +315,8 @@ hangglider = Ride.create(
   address: '6, quai de Gesvres 75004 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://static.sciencelearn.org.nz/images/images/000/000/301/embed/FLT_LKC_ART_04_Hanggliders_HandGlider.jpg?1522293665'
 )
 
 tractor = Ride.create(
@@ -301,7 +326,8 @@ tractor = Ride.create(
   address: '6, quai Watier 78400 Chatou',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQDOaN6-uk8mJbuucuQCDOnCDSHELWMRsDDSBbLNm4pidI1H_kXsyxW2PuNdYnm7p12nX4&usqp=CAU'
 )
 
 barge = Ride.create(
@@ -311,7 +337,8 @@ barge = Ride.create(
   address: '6, rue du Delta 75009 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://parispeniches.fr/wp-content/uploads/2020/08/peniche_albatros_img1-1.jpg'
 )
 
 sidecar = Ride.create(
@@ -321,7 +348,8 @@ sidecar = Ride.create(
   address: '6, rue du Montparnasse 75006 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://www.lac-annecy.com/cache/images/11855334_apidae-ficheGallery.jpg'
 )
 
 tuktuk = Ride.create(
@@ -331,7 +359,8 @@ tuktuk = Ride.create(
   address: '2, rue de Viarmes 75001 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://c8.alamy.com/compfr/fj1g4g/tuk-tuk-taxis-lisbonne-portugal-fj1g4g.jpg'
 )
 
 rocket = Ride.create(
@@ -341,7 +370,8 @@ rocket = Ride.create(
   address: '2, rue de la fontaine au roi 75011 Paris',
   availability: true,
   price_per_day: rand(50..100),
-  user: users.sample
+  user: users.sample,
+  photo: 'https://cdn.mos.cms.futurecdn.net/qTmvroik2NtHVGsfCHyyxF-1200-80.jpg'
 )
 
 puts 'finished !!'
