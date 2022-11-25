4bab192b238daad0ec1620d9d762a4ccc811c4a7
pauline gouttiere <paulinegouttiere@gmail.com>
2022-11-25 15:18:49 +0100

    front show again apres cata

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_11_24_162618) do
+ActiveRecord::Schema[7.0].define(version: 2022_11_25_095857) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -67,6 +67,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_11_24_162618) do
     t.datetime "updated_at", null: false
     t.float "latitude"
     t.float "longitude"
+    t.string "photo"
     t.index ["user_id"], name: "index_rides_on_user_id"
   end
 
